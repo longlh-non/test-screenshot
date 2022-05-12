23945fdf569dba10c02ecbdc4187ff583b0cc65e
longlh-non <longlh1809@gmail.com>
2022-05-12 17:23:04 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <screen-shot/>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -31,7 +32,11 @@
 </template>
 
 <script>
+import ScreenShot from './screen-shot.vue'
 export default {
+  components: {
+    ScreenShot
+  },
   name: 'HelloWorld',
   props: {
     msg: String
